c331063eff3813ca54d2556d58d673b2f1f14bf8
alam01491625 <alam01491625@gmail.com>
2025-04-08 20:04:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,54 +1,27 @@
 var config = {
-    // The length of sentences can be arbitrary, you can write 10 or 20 sentences
-    // Each sentence should preferably be within 15 characters for better display
     texts: [
-        "For my",               
-        "Beloved girl",         
-        "Today is your birthday",
+        "For my friend",               
+        "Shareen Anwar",         
+        "Today is your birthday!",
         "This is our",
-        "Third birthday together!",
-        "Over the past two years, I've seen you",
-        "Acting weird",
-        "Being cute",
-        "Getting angry",
-        "And I love all your sides",
-        "mujhe maaf kr dena",
-        "moka dedo khud ko banane ka apke liye",
-        "In-sha-Allah kabhi esa nhi hoga",
-        "want to hold your hand till my last breath",
-        "Apko kabhi kehne ki zarurat nhi padegi",
-        "Happy Birthday my Rafiya",
+        "First birthday celebration together!",
+        "Over some months, I have seen you",
+        "Always smiling",
+        "Being super cute",
+        "Working so hard",
+        "And always being humble",
+        "You're genuinely inspiring",
+        "Your calm nature is amazing",
+        "Your dreams of teaching will come true In-sha-Allah",
+        "Proud to know someone like you",
+        "Stay the way you are",
+        "Happy Birthday, Shareen!",
     ],
     
-    /**
-     * imgs can be left empty, but if you want to add images, follow the format below
-     * "Matching text from above" : "Image path, you can place images in the imgs folder"
-     * Example
-     * "Beloved girl": "./imgs/girl.jpg"
-     *
-     * If you don't want images, just add two slashes at the beginning of the line
-     * For example, the image for "Today is your birthday" won't be displayed:
-     */
     imgs: {
-        "For my": "./imgs/songxinxin.jpeg",
-        "Beloved girl": "./imgs/nothing.jpg",
-        "Today is your birthday": "./imgs/shengri.jpeg",
-        "This is our": "./imgs/zaiyiqi.png",
-        "Third birthday together!": "./imgs/together.png",
-        "Over the past two years, I've seen you": "./imgs/girl.png",
-        "Acting worried": "./imgs/worried.jpg",
-        "Being cute": "./imgs/cute.jpg",
-        "Getting angry": "./imgs/angry.jpg",
-        "And I love all your sides": "./imgs/heart.jpg",
-        "mujhe maaf kr dena": "./imgs/sorry.jpg",
-        "moka dedo khud ko banane ka apke liye": "./imgs/request.png",
-        "In-sha-Allah kabhi esa nhi hoga": "./imgs/promise.jpg",
-        "want to hold your hand till my last breath": "./imgs/hands.jpg",
-        "Apko kabhi kehne ki zarurat nhi padegi": "./imgs/cutei.png",
-        "Happy Birthday my Rafiya": "./imgs/hijab.jpg"
+        
     },
     
-    // Button text descriptions, you can modify these as you like
     desc: {
         turn_on: "Click to Start",
         play: "Play Sound",
@@ -57,9 +30,8 @@ var config = {
         cake_fadein: "Cake?",
         light_candle: "Candle?",
         wish_message: "Click here Birthday girl",
-        story: "SOME MESSAGE FOR YOU"
+        story: "A SMALL MESSAGE FOR YOU"
     },
 
-    // Closing text
-    loveText: 'Dr. Rafiya Naaz'
+    loveText: 'From Your Friend'
 };
